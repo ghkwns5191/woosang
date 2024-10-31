7a52f9c84ff0bd9f9abfff10537b532083519d7c
ghkwns5191 <ghkwns5191@gmail.com>
2024-10-31 13:56:14 +0900

    컨트롤러 작성 중간 커밋

--- demo/src/main/java/com/example/demo/home/HomeController.java
+++ demo/src/main/java/com/example/demo/home/HomeController.java
@@ -22,6 +22,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
+＠RequestMapping(value = {"", "/"})
 public class HomeController {
 	
 	private static final Logger log = LoggerFactory.getLogger(HomeController.class);
@@ -38,127 +39,70 @@ public class HomeController {
 	 * -- 일단 여기까지 구현하고 미팅 후 추가 개발
 	 * -- ui 는 일단 bootstrap 으로 기본 구현 후 상세 디자인은 추후 적용할 예정.
 	 * (우선 개발 모듈 확인이 우선)
+	 * 현재는 타임리프 사용 중이기 떄문에 return 타입은 String 으로 받는 것이 유리해보임. (추후 vue.js 등으로 변경 검토 필요함. / 양방향 바인딩이 편하기 때문.)
 	 */
 
-	@RequestMapping("/")
-	public String main(HttpServletRequest request, HttpServletResponse response, Model model) {
-		HttpSession session = request.getSession();
-		String sessionVal = session.getId();
-		model.addAttribute("sessionVal", sessionVal);
-		
-		Map<String, Object> datas = new HashMap<>();
-		datas = this.homeService.getDialogs();
-		
-		return "ui/home";
+	/*
+	* 회원가입
+	* 필요한 정보들을 입력
+	* pw 암호화 등 적용하여 (이미 서비스 로직으로 만들어 놓음)
+	* insert 후 로그인 화면으로 이동 처리
+	*/
+
+	// 회원가입 화면으로 이동.
+	@ReqeustMapping(value="/join", method=RequestMethod.GET)
+	public String join(HttpServletRequest request
+						, HttpServletResponse response) {
+
+		return "회원가입 페이지";
 	}
 	
-	
-	@RequestMapping("/chatting")
-	public String chatting(HttpServletRequest request, HttpServletResponse response, Model model) {
-		HttpSession session = request.getSession();
-		String sessionVal = session.getId();
-		model.addAttribute("sessionVal", sessionVal);
+	// 실제 회원가입 로직 호출.
+	@ReqeustMapping(value="/join", method=RequestMethod.POST)
+	public ResponseEntity join(HttpServletRequest request
+						, HttpServletResponse response
+						, @RequestBody Map<String, Object> joinInfo) {
 		
-		Map<String, Object> datas = new HashMap<>();
-//		datas = this.homeService.getDialogs();
-		
-		return "ui/login";
+
+		Map<String, Object> result = this.homeService.join(request, response, joinInfo);
+		return "회원가입 페이지";
 	}
-	
-	
-	@RequestMapping("/login")
-	public String login(HttpServletRequest request, HttpServletResponse response, Model model) {
-		HttpSession session = request.getSession();
-		String sessionVal = session.getId();
-		model.addAttribute("sessionVal", sessionVal);
+
+	/*
+	* 로그인
+	* id / pw 입력 받음.
+	* pw를 복호화 및 입력값과 일치여부 확인 (이미 서비스 로직으로 만들어 놓음)
+	* 성공 시 홈 화면으로 이동.
+	*/
+
+	// 로그인 화면으로 이동.
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(HttpServletRequest request
+						, HttpServletResponse response) {
+
 		
-//		this.homeService.login(request, response);
-//		
-//		Map<String, Object> datas = new HashMap<>();
-//		datas = this.homeService.getDialogs();
-		
-		return "ui/login";
+		return "로그인 화면";
 	}
-	
-	
-	@RequestMapping("/room")
-	public String room(HttpServletRequest request, HttpServletResponse response, Model model) {
-		HttpSession session = request.getSession();
+
+	// 로그인 화면에서 로그인 진행.
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public ResponseEntity<Map<String, Object> login(HttpServletRequest request
+						, HttpServletResponse response
+						, @RequestBody Map<String, Object> loginInfo) {
+
+		Map<String, Object> result = this.homeService.login(request, response, loginInfo);
 		
-		String sessionVal = session.getId();
-		model.addAttribute("sessionVal", sessionVal);
-		
-		log.info(sessionVal);
-		log.info((String) session.getAttribute("username"));
-		Map<String, Object> datas = new HashMap<>();
-//		datas = this.homeService.getDialogs();
-		
-		return "ui/index";
+		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
-	
-	
-	@RequestMapping(value = "/login", method=RequestMethod.POST)
-	public ResponseEntity<String> login(HttpServletRequest request, HttpServletResponse response, @RequestBody Map<String, Object> loginInfo) {
-		boolean result = false;
-		log.info(loginInfo.toString());
-		
-		/*
-		 *  로그인 메서드 시용하여 처리 후 결과 받아서 map 에 담음.
-		 *  처리 후에는 
-		 *  1. 로그인 문제없는지 여부
-		 *  2. 로그인 후 갱신한 세션 값
-		 *  3. 로그인에 성공한 username --> ID 값으로 변경할까 고민 중.
-		 *  정보를 반환.
-		 */
-		Map<String, Object> loginResult = this.homeService.login(request, response, loginInfo);
-		log.info(loginResult.toString());
-		
-		
-		if ((boolean) loginResult.get("loginFlag")) {
-			result = true;
-			
-			
-			HttpSession newSession = (HttpSession) loginResult.get("session");
-			newSession.setAttribute("username", Util.mapToString(loginResult, "username"));
-			log.info(newSession.toString());
-			
-			// 로그 인 성공 후에는 세션값을 갱신하고 쿠기 값으로 username 을 함께 전송해둠.
-			Cookie cookie = new Cookie("username", Util.mapToString(loginResult, "username"));
-			response.addCookie(cookie);  // 쿠키를 클라이언트에 추가
-			log.info(cookie.toString());
-		} else {
-			result = false;
-		}
-		String res = "";
-		
-		if (result) {
-			res = "success";
-		} else {
-			res = "fail";
-		}
-		
-		
-		return new ResponseEntity<>(res, HttpStatus.OK);
-	}
-	
-	
-	@RequestMapping(value = "/join", method=RequestMethod.POST)
-	public ResponseEntity<String> join(HttpServletRequest request, HttpServletResponse response, @RequestBody Map<String, Object> joinInfo) {
-		boolean result = false;
-		
-		Map<String, Object> joinResult = this.homeService.join(request, response, joinInfo);
-		if ((boolean) joinResult.get("joinFlag")) {
-			result = true;
-		} else {
-			result = false;
-		}
-		String res = "";
-		if (result) {
-			res = "success";
-		} else {
-			res = "fail";
-		}
-		
-		return new ResponseEntity<>(res, HttpStatus.OK);
-	}
+
+	/*
+	* 이력서 조회
+	* 해당 인원의 해당 이력정보를 가져옴.
+	* (일반 Map 데이터와 List<Map> 데이터를 모두 뽑아내어 model 데이터로 전송 (Response))
+	* pw를 복호화 및 입력값과 일치여부 확인 (이미 서비스 로직으로 만들어 놓음)
+	* 성공 시 홈 화면으로 이동.
+	*/
+
+
+	// 
 }
