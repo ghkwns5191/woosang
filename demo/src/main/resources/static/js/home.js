c6a897a2205997ab4523c8ccfea788914d301798
ghkwns5191 <ghkwns5191@gmail.com>
2024-11-11 21:25:25 +0900

    로그아웃 완성

--- demo/src/main/resources/static/js/home.js
+++ demo/src/main/resources/static/js/home.js
@@ -9,3 +9,39 @@ function toSignUp() {
 function toResume() {
     window.location.href = "/resume/list";
 }
+
+function toLogOut() {
+	
+	
+		$.ajax({
+	 	url : "/logout"
+	 	, type : 'post'
+	 	, headers : {              // Http header
+       "Content-Type" : "application/json",
+       "X-HTTP-Method-Override" : "POST"
+     }
+     , dataType : 'json'
+	 	, success : function (data) {
+			console.log(data);
+	 		console.log(data.loginFlag);
+		
+	 		if (data.logoutInfo) {
+	 			console.log(window.location.origin);
+	 			window.alert("로그아웃 하였습니다.");
+	 			window.location.href="/";
+	 		} else if (!data.logoutInfo) {
+	 			window.alert("아이디 혹은 비밀번호가 잘못되었습니다.");
+	 			document.getElementById("username").value = "";
+	 			document.getElementById("password").value = "";
+	 		}
+		
+	 	}
+	 	, error : function() {
+
+	 	}
+	 	, complete: function() {
+		
+	 	}
+	 });
+	
+}
